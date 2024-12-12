30fb5bf9fec66e0776183e085d15e8308f991fab
SyrillDex <syrilldexl@gmail.com>
2024-12-12 17:42:41 +0800

    Manipulate the string to separate the value and the symbol and then create a method for converting temperature to F

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    String tempInput;
+    String conversionTypeInput;
+    float result;
+
     System.out.println("Welcome to Temperature Converter app");
+    System.out.println();
+
+    System.out.println("Temperature to convert (include C, F, or K at the end): ");
+    tempInput = scan.nextLine().trim();
+
+    int tempLength = tempInput.length();
+    String centigrade = tempInput.substring(tempLength - 1).toUpperCase();
+    String tempValue = tempInput.substring(0, tempLength - 1);
+    double temp;
+
+    if (!centigrade.equals("C") && !centigrade.equals("F") && !centigrade.equals("K")) {
+      System.out.println("Please enter a valid centigrade");
+      return;
+    }
+
+    try {
+      temp = Double.parseDouble(tempValue);
+    }
+    catch (NumberFormatException e) {
+      System.out.println("Please enter a valid number");
+      return;
+    }
+
+    System.out.println("Convert to? (C, F, or K) ");
+    conversionTypeInput = scan.nextLine().toUpperCase().trim();
+
+    switch (conversionTypeInput) {
+      case "F":
+        switch (centigrade) {
+          case "C":
+            result = (float) ((temp * 9/5) + 32);
+            System.out.println(result + " " + conversionTypeInput);
+            break;
+          case "K":
+            result = (float) ((temp - 273.15) * 9/5 + 32);
+            System.out.println(result + " " + conversionTypeInput);
+            break;
+          case "F":
+            System.out.println(temp + " " + conversionTypeInput);
+            break;
+        }
+        break;
+      case "C":
+        System.out.println(temp);
+        break;
+      case "K":
+        System.out.println(temp);
+        break;
+      default:
+        System.out.println("Please enter a valid centigrade");
+    }
+
   }
 }
